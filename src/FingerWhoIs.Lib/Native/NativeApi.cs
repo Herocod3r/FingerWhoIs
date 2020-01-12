4be354518786d42d5f66060ecb85ea94fb38de40
Abdulrahman Sulaiman <amansulaiman92@gmail.com>
2020-01-12 13:31:31 +0100

    refactoring

--- src/FingerWhoIs.Lib/Native/NativeApi.cs
+++ src/FingerWhoIs.Lib/Native/NativeApi.cs
@@ -139,14 +139,7 @@ namespace FingerWhoIs.Lib.Native
             Span<int> fmdSizes = fmds.Length <= 128 ? stackalloc int[fmds.Length] : new int[fmds.Length];
             for (int i = 0; i < fmds.Length; i++)
             {
-                if (fmds[i] == null)
-                {
-                    fmdSizes[i] = 0;
-                }
-                else
-                {
-                    fmdSizes[i] = fmds[i].Length;
-                }
+                fmdSizes[i] = fmds[i].Length;
             }
 
             int call = 0;
