a06505ee71ed02caf73fe040d115ee0480ef60ef
herocod3r <jethromain@gmail.com>
2020-01-13 03:42:02 +0100

    added test project and modified the dockerfile

---
+++ src/FingerWhoIs.Test/FingerEngineTests.cs
@@ -0,0 +1,58 @@
+using System;
+using FingerWhoIs.Lib;
+using Xunit;
+
+namespace FingerWhoIs.Test
+{
+    public class FingerEngineTests
+    {
+        [Theory]
+        [InlineData("aGVsbG8=")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Test_ValidateFmd_Invalid(string print)
+        {
+            var fmd = !string.IsNullOrEmpty(print) ? Convert.FromBase64String(print) : null;
+            var engine = new FingerEngine();
+
+            var result = engine.ValidateFMD(fmd);
+            Assert.False(result);
+
+        }
+
+
+        [Theory]
+        [InlineData("Rk1SACAyMAAAAADYAAABAAFoAMgAyAEAAABcH4CdAD6/AIB5AGXIAICYAG1HAICuAG3OAIDNAHLUAEBoAHTDAEC4AJFOAECBAJZNAECqAJnbAIA4AKjJAIDiAMBtAIDCAMTtAIBWAM7eAIBsANlbAIAUAO3TAECOAQRqAIDIAQXyAEBhAQnoAICKARbrAEDoAST4AECxASZuAEClATJuAIA4ATTeAIAZATndAIA9AUVZAICCAU1rAEBaAVLqAEB9AVTxAECkAVR9AICYAVbzAICYAVpwAAAA")]
+        public void Test_ValidateFmd_Valid(string print)
+        {
+            var fmd = !string.IsNullOrEmpty(print) ? Convert.FromBase64String(print) : null;
+            var engine = new FingerEngine();
+
+            var result = engine.ValidateFMD(fmd);
+            Assert.True(result);
+        }
+
+
+
+        [Fact]
+        public void Test_CompareFmds()
+        {
+            var fmd = Convert.FromBase64String("Rk1SACAyMAAAAADYAAABAAFoAMgAyAEAAABcH4CdAD6/AIB5AGXIAICYAG1HAICuAG3OAIDNAHLUAEBoAHTDAEC4AJFOAECBAJZNAECqAJnbAIA4AKjJAIDiAMBtAIDCAMTtAIBWAM7eAIBsANlbAIAUAO3TAECOAQRqAIDIAQXyAEBhAQnoAICKARbrAEDoAST4AECxASZuAEClATJuAIA4ATTeAIAZATndAIA9AUVZAICCAU1rAEBaAVLqAEB9AVTxAECkAVR9AICYAVbzAICYAVpwAAAA");
+            var engine = new FingerEngine();
+            var result = engine.CompareTwoFmds(fmd,fmd);
+
+            Assert.True(result <= 21474);
+        }
+        
+        [Fact]
+        public void Test_IdentifySingleFmd()
+        {
+            var fmd = Convert.FromBase64String("Rk1SACAyMAAAAADYAAABAAFoAMgAyAEAAABcH4CdAD6/AIB5AGXIAICYAG1HAICuAG3OAIDNAHLUAEBoAHTDAEC4AJFOAECBAJZNAECqAJnbAIA4AKjJAIDiAMBtAIDCAMTtAIBWAM7eAIBsANlbAIAUAO3TAECOAQRqAIDIAQXyAEBhAQnoAICKARbrAEDoAST4AECxASZuAEClATJuAIA4ATTeAIAZATndAIA9AUVZAICCAU1rAEBaAVLqAEB9AVTxAECkAVR9AICYAVbzAICYAVpwAAAA");
+            var engine = new FingerEngine();
+            var result = engine.IdentifyFmd(fmd,new [] {fmd});
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
